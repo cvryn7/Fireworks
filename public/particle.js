2dfacb17735f949d510a5d9b77e96bcdb8de0aa5
karanbhagat <cv.ryn7@gmail.com>
2017-08-05 04:33:49 -0400

    Created a particle with bare minimum physics

---
+++ public/particle.js
@@ -0,0 +1,37 @@
+/**
+ * Constructor function for creating a
+ * particle
+ * @param x
+ * @param y
+ * @constructor
+ */
+function Particle(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+
+    //force accumulation
+    //force = mass * acceleration
+    //I am eliminating mass for simplicity
+    this.applyForce = function(force) {
+        this.acceleration.add(force);
+
+    }
+
+    this.update = function () {
+        //adds acceleration to the velocity
+        this.velocity.add(this.acceleration);
+
+        //adds velocity to the position
+        this.position.add(this.velocity);
+
+        //clear out acceleration since
+        //we need to start for each frame
+        //with 0 acceleration
+        this.acceleration.mult(0);
+    }
+
+    this.show = function() {
+        point(this.position.x, this.position.y);
+    }
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,17 @@
+var firework;
+
 function setup() {
     createCanvas(400, 300);
+
+    //if we set stroke or any style element here
+    //p5js can render lot faster
+    stroke(255);
+    strokeWeight(4);
+    firework = new Particle(200, 150);
 }
 
 function draw() {
     background(51);
+    firework.update();
+    firework.show();
 }
